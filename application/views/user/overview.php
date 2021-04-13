13f9b21706ba0bfd58cecddb650b1fae20b87285
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-04-13 14:52:07 +0700

    template

---
+++ application/views/user/overview.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Home - Brand</title>
+  <link rel="stylesheet" href="<?php echo base_url('assets/assets/bootstrap/css/bootstrap.min.css')?>">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abel">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abhaya+Libre">
+  <link rel="stylesheet" href="<?php echo base_url('assets/assets/fonts/font-awesome.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/assets/css/untitled-1.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/assets/css/untitled.css')?>">
+</head>
+
+<body>
+  <nav class="navbar navbar-light navbar-expand-sm fixed-top bg-white text-monospace shadow-lg navigation-clean">
+    <div class="container"><a class="navbar-brand" href="#">Brand</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"></button>
+      <div class="collapse navbar-collapse" id="navcol-1"><a class="btn btn-primary ml-auto" role="button" href="#">Sign In</a></div>
+    </div>
+  </nav>
+  <header class="text-center text-white masthead" style="background: url(&quot;<?php echo base_url('assets/assets/img/1-11.jpg')?>&quot;) top / cover;">
+    <div class="d-inline-block overlay" style="background: rgb(55,100,146);"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class=" text-center mb-0">
+            PASANG-NEGO-BERES
+          </h1>
+          <h3 class="text-center">
+            Mudah, Cepat, Efisien
+          </h3>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form>
+            <div class="form-row">
+              <div class="col">
+                <div class="input-group">
+                  <div class="input-group-prepend"></div><input class="form-control" type="text" placeholder="Apa ?"><input class="form-control" type="text" placeholder="Dimana ?">
+                  <div class="input-group-append"><button class="btn btn-primary text-left" type="button">Go!<i class="fa fa-search float-left d-xl-flex justify-content-xl-center align-items-xl-center"
+                        style="transform: translate(-2px) translateX(-2px) translateY(3px);"></i></button></div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container">
+    <section style="background: #e9ecef;margin: 10px;padding: 10px;border-style: none;border-radius: 5px;box-shadow: 0px 0px 0px rgb(27,30,33);">
+      <div class="container-lg" style="margin: 10px;">
+        <div class="row">
+          <div class="col">
+            <h1>Pilih kota atau daerah</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="row">
+              <div class="col">
+                <form>
+                  <div class="form-row">
+                    <div class="col-md-3">
+                      <ul class="list-unstyled">
+                        <li>Jakarta</li>
+                        <li>Bandung</li>
+                        <li>Medan</li>
+                        <li>Suarabaya</li>
+                        <li>Bekasi</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul class="list-unstyled">
+                        <li>Palembang</li>
+                        <li>Makassar</li>
+                        <li>Tanggerang</li>
+                        <li>South Tanggerang</li>
+                        <li>Semarang</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul class="list-unstyled">
+                        <li>Depok</li>
+                        <li>Batam</li>
+                        <li>Padang</li>
+                        <li>Denpasar</li>
+                        <li>Kota Lainnya</li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3 col-xl-3" style="transform: translateX(164px);"><img src="<?php echo base_url('assets/assets/img/c12261c78e0a35b60cceb2315d3a277b.png')?>" width="300px" style="transform: translateX(-197px);"></div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <footer class="bg-light footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 text-center text-lg-left my-auto h-100">
+          <ul class="list-inline mb-2">
+            <li class="list-inline-item"><a href="#">About</a></li>
+            <li class="list-inline-item"><span>⋅</span></li>
+            <li class="list-inline-item"><a href="#">Contact</a></li>
+            <li class="list-inline-item"><span>⋅</span></li>
+            <li class="list-inline-item"><a href="#">Terms of &nbsp;Use</a></li>
+            <li class="list-inline-item"><span>⋅</span></li>
+            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+          </ul>
+          <p class="text-muted small mb-4 mb-lg-0">© Brand 2021. All Rights Reserved.</p>
+        </div>
+        <div class="col-lg-6 text-center text-lg-right my-auto h-100">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook fa-2x fa-fw"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter fa-2x fa-fw"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fa fa-instagram fa-2x fa-fw"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src="<?php echo base_url('assets/js/jquery.min.js')?>"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
+</body>
+
+</html>
