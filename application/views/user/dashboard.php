d52580702ce913c986de59d1002f081ac6fdff72
Akhmad Nur Hidayatulloh <07akhmadnur@gmail.com>
2021-05-01 23:51:18 +0700

    Edit dashboard user - Collapse

--- application/views/user/dashboard.php
+++ application/views/user/dashboard.php
@@ -9,72 +9,24 @@
     <main class="container mb-4" style="margin-top: 70px;">
       <div class="row">
         <div class="col-md-3 mb-3">
-          <!-- Card -->
-          <div class="card">
-            <div class="card-body bg-light">
-             <!-- My Account -->
-              <div class="row">
-                <div class="col text-left">
-                  AKUN SAYA
-                </div>
-                <div class="col text-right">
-                  <a href="#myAccount" role="button" aria-expanded="false" aria-controls="myAccount" data-toggle="collapse">
-                    <i class="bi bi-caret-right-fill"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="collapse" id="myAccount"> 
-                <ul class="list-group">
-                  <li class="list-group-item">
-                    <a href="#">
-                      <i class="bi bi-house-fill"></i> Beranda Saya
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <hr>
-              <!-- MY ADS -->
-              <div class="row">
-                <div class="col text-left">
-                  IKLAN SAYA
-                </div>
-                <div class="col text-right">
-                  <a href="#myAds" role="button" aria-expanded="false" aria-controls="myAds" data-toggle="collapse">
-                      <i class="bi bi-caret-right-fill"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="collapse" id="myAds">
-                <ul class="list-group">'
-                  <li class="list-group-item">
-                    <i class="bi bi-badge-ad"></i> Iklan Saya <div class="badge badge-secondary">0</div>
-                  </li>
-                  <li class="list-group-item">
-                    <i class="bi bi-clock"></i> Menunggu Persetujuan <div class="badge badge-secondary">0</div>
-                  </li>
-                  <li class="list-group-item">
-                    <i class="bi bi-cash"></i> Transaksi <div class="badge badge-secondary">0</div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <!-- Sidebar -->
+          <?php $this->load->view("user/_partials/dashboard_sidebar") ?>
         </div>
 
         <!-- Col 2 - Main content -->
         <div class="col-md-9">
-          <!-- Summary -->
+          <!-- Summary
           <div class="card">
             <div class="card-body p-3 bg-light">
               <div class="row">
                 <div class="col-md-10">
                   <a href="">
                     <img src="https://placeimg.com/50/50/people" alt="" style="width: 65px; border: solid 2px #ddd; padding: 2px; vertical-align: middle">
-                    <span style="font-size: large;" class="ml-1">Akhmad Nur Hidayatulloh</span>
+                    <span style="font-size: 18px;" class="ml-1">Akhmad Nur Hidayatulloh</span>
                   </a>
                 </div>
                 <div class="col-md-2 text-right" style="display: inline-block">
-                  <div id="col-left" ">
+                  <div id="col-left">
                     <i class="bi bi-badge-ad-fill" style="font-size: 3rem"></i>
                   </div>
                   <div id="col-right">
@@ -84,21 +36,25 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
           <!-- Account Management -->
-          <div class="card mt-3">
+          <div class="card">
             <div class="card-body p-3 bg-light">
-              <h3>Halo Akhmad Nur Hidayatulloh</h3>
+              <h4>Halo, Akhmad Nur Hidayatulloh</h4>
               <!-- Avatar -->
               <div class="card mt-2">
-                <div class="card-header">
-                  Photo or Avatar
-                </div>
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-md-12 text-center">
-                      <div class="image">
-                        <img src="https://placeimg.com/50/50/people" alt="" style="width: 200px; padding: 5px; border: solid 1px #ddd">
+                <a href="#avatarCollapse" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="avatarCollapse" class="list-group-item-action">
+                  <div class="card-header">
+                    Photo or Avatar
+                  </div>
+                </a>
+                <div class="collapse show" id="avatarCollapse">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-md-12 text-center">
+                        <div class="image">
+                          <img src="<?php echo 'https://placeimg.com/50/50/people' ?>" alt="" style="width: 200px; padding: 5px; border: solid 1px #ddd">
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -106,72 +62,94 @@
               </div>
               <!-- Account Details -->
               <div class="card mt-3">
-                <div class="card-header">
-                  Account Details
-                </div>
-                <div class="card-body" style="padding: 45px;">
-                  <form action="#" method="POST">
-                    <!-- <div class="form-group row">
-                      <div class="col-md-9">
-                        <input type="file" name="avatar" id="avatar" class="form-control">
-                      </div>
-                    </div> -->
-                    <div class="form-group row">
-                      <label for="" class="col-sm-3 col-form-label">Photo or Avatar</label>
-                      <div class="col-sm-9">
-                        <div class="custom-file">
-                          <input type="file" name="avatar" id="avatar" class="custom-file-input">
-                          <label for="avatar" class="custom-file-label">Photo or Avatar</label>
+                <a href="#accountCollapse" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="accountCollapse" class="list-group-item-action">
+                  <div class="card-header">
+                    Account Details
+                  </div>
+                </a>
+                <div class="collapse show" id="accountCollapse">
+                  <div class="card-body" style="padding: 45px;">
+                    <form action="#" method="POST">
+                      <!-- <div class="form-group row">
+                        <div class="col-md-9">
+                          <input type="file" name="avatar" id="avatar" class="form-control">
+                        </div>
+                      </div> -->
+                      <div class="form-group row">
+                        <label for="avatar" class="col-sm-3 col-form-label">Photo or Avatar</label>
+                        <div class="col-sm-9">
+                          <div class="custom-file">
+                            <input type="file" name="avatar" id="avatar" class="custom-file-input">
+                            <label for="avatar" class="custom-file-label">Photo or Avatar</label>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    
-                    <div class="form-group row">
-                      <label for="nama" class="col-sm-3 col-form-label">Nama<sup style="color: tomato" >*</sup></label>
-                      <div class="col-sm-9">
-                        <input type="text" name="nama" id="nama" placeholder="Nama Lengkap" class="form-control" required>
+                      
+                      <div class="form-group row">
+                        <label for="nama" class="col-sm-3 col-form-label">Nama<sup style="color: tomato" >*</sup></label>
+                        <div class="col-sm-9">
+                          <input type="text" name="nama" id="nama" placeholder="Nama Lengkap" class="form-control" value="<?php echo 'Akhmad Nur Hidayatulloh' ?>" required>
+                        </div>
                       </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="email" class="col-sm-3 col-form-label">Email<sup style="color: tomato" >*</sup></label>
-                      <div class="col-sm-9">
-                        <input type="email" name="email" id="email" class="form-control" placeholder="Email" required >
+                      <div class="form-group row">
+                        <label for="gender" class="col-sm-3 col-form-label">Jenis Kelamin<sup style="color: tomato" >*</sup></label>
+                        <div class="col-sm-9">
+                          <div class="form-check form-check-inline">
+                            <input type="radio" name="gender" id="gender1" class="form-check-input" value="Laki-Laki" checked>
+                            <label for="gender1" class="form-check-label">Laki-Laki</label>
+                          </div>
+                          <div class="form-check form-check-inline">
+                            <input type="radio" name="gender" id="gender2" class="form-check-input" value="Perempuan">
+                            <label for="gender2" class="form-check-label">Perempuan</label>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="telp" class="col-sm-3 col-form-label">Telepon<sup style="color: tomato" >*</sup></label>
-                      <div class="col-sm-9">
-                        <input type="text" name="telp" id="telp" class="form-control" placeholder="Nomor Telepon" required >
+                      <div class="form-group row">
+                        <label for="email" class="col-sm-3 col-form-label">Email<sup style="color: tomato" >*</sup></label>
+                        <div class="col-sm-9">
+                          <input type="email" name="email" id="email" class="form-control" placeholder="Email" value="<?php echo 'aaa@gmail.com' ?>" required >
+                        </div>
                       </div>
-                    </div>
-                    <div class="row pr-3">
-                      <button type="submit" class="btn btn-primary offset-md-10 col">Perbarui</button>
-                    </div>
+                      <div class="form-group row">
+                        <label for="telp" class="col-sm-3 col-form-label">Telepon<sup style="color: tomato" >*</sup></label>
+                        <div class="col-sm-9">
+                          <input type="text" name="telp" id="telp" class="form-control" placeholder="Nomor Telepon" value="<?php echo '088888888' ?>" required>
+                        </div>
+                      </div>
+                      <div class="row pr-3">
+                        <button type="submit" class="btn btn-primary offset-md-10 col">Perbarui</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
               <!-- Pengaturan -->
               <div class="card mt-3">
-                <div class="card-header">
-                  Pengaturan
-                </div>
-                <div class="card-body" style="padding: 45px;">
-                  <form action="#" method="post">
-                    <div class="form-group row">
-                      <label for="pwd" class="col-md-3 col-form-label">Password Baru<sup style="color: tomato" >*</sup></label>
-                      <div class="col-md-9">
-                        <input type="password" name="pwd" id="pwd" class="form-control" placeholder="Password" required>
+                <a href="#settingCollapse" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="settingCollapse" class="list-group-item-action">
+                  <div class="card-header">
+                    Pengaturan
+                  </div>
+                </a>
+                <div class="collapse show" id="settingCollapse">
+                  <div class="card-body" style="padding: 45px;">
+                    <form action="#" method="post">
+                      <div class="form-group row">
+                        <label for="pwd" class="col-md-3 col-form-label">Password Baru<sup style="color: tomato" >*</sup></label>
+                        <div class="col-md-9">
+                          <input type="password" name="pwd" id="pwd" class="form-control" placeholder="Password" required>
+                        </div>
                       </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="pwdC" class="col-md-3 col-form-label">Konfirmasi Password<sup style="color: tomato" >*</sup></label>
-                      <div class="col-md-9">
-                        <input type="password" name="pwdC" id="pwdC" class="form-control" placeholder="Konfirmasi Password" required>
+                      <div class="form-group row">
+                        <label for="pwdC" class="col-md-3 col-form-label">Konfirmasi Password<sup style="color: tomato" >*</sup></label>
+                        <div class="col-md-9">
+                          <input type="password" name="pwdC" id="pwdC" class="form-control" placeholder="Konfirmasi Password" required>
+                        </div>
                       </div>
-                    </div>
-                    <div class="row pr-3">
-                      <button type="submit" class="btn btn-primary offset-md-10 col">Perbarui</button>
-                    </div>
-                  </form>
+                      <div class="row pr-3">
+                        <button type="submit" class="btn btn-primary offset-md-10 col">Perbarui</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
