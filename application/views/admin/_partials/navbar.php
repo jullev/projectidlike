724484567ff10510976dc670bf22891435f4ff25
Hanif Rimamtomo <65529407+Shortfire13@users.noreply.github.com>
2021-05-06 20:26:42 +0700

    update admin

--- application/views/admin/_partials/navbar.php
+++ application/views/admin/_partials/navbar.php
@@ -1,6 +1,6 @@
 <!-- navbar  -->
 <nav class="main-header navbar navbar-expand navbar-dark navbar-light">
-	<button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" data-widget="pushmenu" href="#sidebar"><i
+	<button class="btn nav-item btn-sm order-1 order-lg-0" style="color: white;" id="sidebarToggle" data-widget="pushmenu" href="#sidebar"><i
 				class="fas fa-bars"></i></button>
 	<a class="navbar-brand" href="<?php echo site_url('admin') ?>"><?php echo SITE_NAME ?></a>
 	<!-- Navbar Search-->
