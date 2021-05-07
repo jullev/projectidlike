60940fb07dbb6471deb8e039f3516b5131d4a676
dimpangg <72178046+dimpangg@users.noreply.github.com>
2021-05-08 04:33:40 +0700

    List Admin: Responsive, Pagination, Search, Sort

--- application/views/admin/_partials/head.php
+++ application/views/admin/_partials/head.php
@@ -26,6 +26,10 @@
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <!-- Bootstrap Icon -->
   <link rel="stylesheet" href="<?php echo base_url('assets/assets/bootstrap-icons/font/bootstrap-icons.css') ?>">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>">
 
 	<!-- Cek apakah sudah login dan role adalah admin -->
 	<?php
