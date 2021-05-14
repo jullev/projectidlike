26c7a2df3d476bafdd32cd1ba65682e993d4c3ca
dimpangg <72178046+dimpangg@users.noreply.github.com>
2021-05-15 00:50:37 +0700

    Detail Page Recode Minus Banyak, Belum Selesai: Responsive

--- application/views/user/detail.php
+++ application/views/user/detail.php
@@ -1,184 +1,114 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-<?php $this->load->view("user/_partials/head.php") ?>
 
-<!-- Font Awesome -->
+<!-- Head -->
+<?php $this->load->view('user/_partials/head.php'); ?>
 
+<body>
 
-<body class="layout-fixed">
-<!-- Navbar -->
-<?php $this->load->view("user/_partials/navbar.php") ?>
-<main role="main" class="container" style="margin-top:70px; margin-bottom: 30px;">
-	<div class="row">
-		<div class="col-lg-9 mb-3">
-			<div class="card">
-				<?php
-				var_dump(@$dataIklan);
-				$judul = @$dataIklan->judul_kerjaan; ?>
-	<?php $this->load->view("user/_partials/head.php") ?>
-	<!-- Font Awesome -->
-	<body class="layout-fixed">
-		<!-- Navbar -->
-		<?php $this->load->view("user/_partials/navbar.php") ?>
-		<main role="main" class="container" style="margin-top:70px; margin-bottom: 30px;">
-			<div class="row">
-				<div class="col-lg-9 mb-3">
-					<span class="info-row" style="color: grey;">
-						<br>
-						<i class="fas fa-home"></i>
-						&nbsp;/&nbsp;
-						<span>Detail Iklan</span>
-						&nbsp;/&nbsp;
-						<span><?php echo @$dataIklan->judul_kerjaan; ?></span>
-						<br>
-						<br>
-					</span>
-					<div class="card">
-						<div class="card-body bg-light">
-							<h2 style="color: #4682b4;"><?php echo @$dataIklan->judul_kerjaan; ?></h2>
-							<hr>
-							<span class="info-row" style="color: grey;">
-								<i class="far fa-clock"></i>
-								&nbsp;
-								<span> deadline <?php echo @$dataIklan->deadline; ?></span>
-								-&nbsp;
-								<i class="fas fa-map-marker-alt"></i>
-								&nbsp;
-								<span>Jember</span>
-								-&nbsp;
-								<i class="fas fa-eye"></i>
-								&nbsp;
-								<span>500x dilihat</span>
-							</span>
-							<!-- Images -->
-							<br>
-							<br>
-							<img src="https://placeimg.com/640/480/any" alt="" style="width:95%">
-							</center>
-							<br>
-							<br>
-							<center>
-							<div class="product-viewer-thumb-wrapper">
-								<ul class="product-view-thumb m-3">
-									<img src="https://placeimg.com/640/480/any" alt="" style="width:20%;">
-									<img src="https://placeimg.com/640/480/any" alt="" style="width:20%;">
-									<img src="https://placeimg.com/640/480/any" alt="" style="width:20%;">
-									<img src="https://placeimg.com/640/480/any" alt="" style="width:20%;">
-								</ul>
-							</div>
-							</center>
-							<!-- Details -->
-							<div id="detail" class="mt-4">
-								<ul class="nav nav-tabs">
-									<li class="nav-item">
-										<a href="#detail" class="nav-link active">Detail Iklan</a>
-									</li>
-								</ul>
-								<div class="tab-content p-4 " style="background-color:#fff">
-									<div class="row">
-										<div class="col-md-6">
-											<i class="fas fa-map-marker-alt"></i>
-											<span>Jakarta</span>
-										</div>
-										<div class="col-md-6 text-right">
-											<span><strong>Harga:</strong> <?php
-												echo number_format(@$dataIklan->harga,2); ?></span>
-										</div>
-									</div>
-									<hr>
-									<div class="row">
-										<div class="col-md-12">
-											<?php echo @$dataIklan->deskripsi; ?> <br>
-											Thank you!
-										</div>
-									</div>
-									<!-- Icons -->
-									<div class="row text-center mt-4">
-										<!-- Kirim pesan -->
-										<div class="col-md-4 col-sm-4">
-											<a href="#">
-												<i class="fas fa-envelope fa-2x" title="Kirim Email"></i>
-											</a>
-										</div>
-										<!-- Simpan iklan -->
-										<div class="col-md-4 col-sm-4">
-											<a href="#">
-												<i class="far fa-heart fa-2x" title="Simpan Iklan"></i>
-											</a>
-										</div>
-										<!-- Laporkan penyalahgunaan -->
-										<div class="col-md-4 col-sm-4">
-											<a href="#">
-												<i class="fas fa-info-circle fa-2x" title="Laporkan Penyalahgunaan"></i>
-											</a>
-										</div>
-									</div>
-								</div>
-							</div>
+	<!-- Navbar -->
+	<?php $this->load->view('user/_partials/navbar.php'); ?>
+
+	<!-- Container -->
+	<div class="container mb-5" style="margin-top: 100px;">
+		<!-- Row -->
+		<div class="row">
+			<!-- Breadcrumb -->
+			<div class="col-md-12 col-lg-9 pr-lg-2 mb-2 mb-lg-0">
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="#"><i class="bi-house-fill"></i></a></li>
+						<li class="breadcrumb-item"><a href="#">Kategori</a></li>
+						<li class="breadcrumb-item active" aria-current="page">Judul</li>
+					</ol>
+				</nav>
+			</div>
+			<div class="col-md-12 col-lg-3 pl-lg-2"></div>
+			<!-- Breadcrumb End -->
+
+			<!-- Main Content -->
+			<div class="col-md-12 col-lg-9 pr-lg-2 mb-3 mb-lg-0">
+				<div class="card">
+					<div class="card-body bg-light">
+						<h3>Ini Judul Ini Judul Ini Judul Ini Judul Ini Judul Ini Judul Ini Judul Ini Judul</h3>
+						<hr>
+						<span class="text-muted small">
+							<i class="bi-clock mr-1"></i> 1 Minggu yang lalu
+							- Kategori
+							- <i class="fas fa-map-marker-alt ml-1 mr-1"></i> Bondowoso
+							- <i class="bi-eye-fill ml-1 mr-1"></i> 50x dilihat
+						</span>
+						<center>
+							<img src="https://placeimg.com/640/480/arch" alt="Ini Judul" title="Ini Judul" class="mt-3 mb-3 w-100">
+							<ul class="list-inline">
+								<img src="https://placeimg.com/640/480/arch" alt="Ini foto 1" title="ini foto 1" style="width: 15%;">
+								<img src="https://placeimg.com/640/480/arch" alt="Ini foto 1" title="ini foto 1" style="width: 15%;">
+								<img src="https://placeimg.com/640/480/arch" alt="Ini foto 1" title="ini foto 1" style="width: 15%;">
+								<img src="https://placeimg.com/640/480/arch" alt="Ini foto 1" title="ini foto 1" style="width: 15%;">
+							</ul>
+						</center>
+						<ul class="nav nav-tabs">
+							<li class="nav-item">
+								<span class="nav-link active"><strong>Detail</strong></span>
+							</li>
+						</ul>
+						<div class="tab-content bg-white p-3" style="border: 1px solid #dee2e6; border-top: none;">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam autem officia vel doloremque tempora id impedit eum repellat. Nulla non qui quam et eos, maiores soluta sit ea nisi. Corporis!
 						</div>
 					</div>
 				</div>
-				<!-- Column 2 -->
-				<div class="col-lg-3">
-					<div class="card">
-						<ul class="list-group">
+			</div>
+			<!-- Main Content End -->
+
+			<!-- Sidebar -->
+			<div class="col-md-12 col-lg-3 pl-lg-2">
+				<div class="card">
+					<div class="card-body py-2 px-0">
+						<ul class="list-group list-group-flush">
 							<!-- Posted by -->
 							<li class="list-group-item">
 								<div class="row">
-									<div class="col-sm-3">
-										<img src="https://placeimg.com/300/300/people" alt="" style="width:50px">
+									<div class="col-lg-4 col-md-2 col-3">
+										<img src="https://placeimg.com/300/300/people" alt="Anggito Suryo" title="Anggito Suryo" class="w-100" style="max-width: 70px;">
 									</div>
-									<div class="col-sm-9">
+									<div class="col-lg-8 col-md-10 col-9 pl-0">
 										<div class="row">
-											<div class="col-sm-12">Posted by:</div>
-											<div class="col-sm-12">Anggito Suryo Baskoro</div>
+											<div class="col-12 text-muted">Posted by</div>
+											<div class="col-12">Anggito Suryo Baskoro Kuncoro Santoso</div>
 										</div>
 									</div>
-								</div>
+								</div>					
 							</li>
+							<!-- Posted by End -->
+							<!-- Lokasi -->
 							<li class="list-group-item">
 								<div class="row">
-									<div class="col-sm-6">
-										<i class="fas fa-map-marker-alt"></i>
-										<span>Lokasi</span>
+									<div class="col-6">
+										<i class="fas fa-map-marker-alt mr-2"></i>
+										<span class="text-muted">Lokasi</span>
 									</div>
-									<div class="col-sm-6 text-right">
-										<a href="#">Jakarta</a>
+									<div class="col-6 text-right">
+										<a href="#">Bondowoso</a>
 									</div>
 								</div>
 							</li>
+							<!-- Lokasi End -->
+							<!-- Button -->
 							<li class="list-group-item">
-								<button class="btn btn-warning mb-1" style="width:100%;"  onclick="location.href='<?php echo base_url() ?>hitiklan/<?php echo @$dataIklan->idkerjaan ?>'" type="button">
-								<i class="far fa-hand-point-up"></i>
-								<span>Hit</span>
-								</button>
-								<button class="btn btn-success" style="width:100%;">
-								<i class="fas fa-phone"></i>
-								<span>+628********</span>
-								</button>
+								<button class="btn btn-warning btn-block"><i class="bi-hand-index-thumb-fill mr-1"></i> Hit</button>
+								<button class="btn btn-success btn-block"><i class="bi-telephone-fill mr-1"></i> +62xxxxxxxxxxx</button>
 							</li>
-						</ul>
-					</div>
+							<!-- Button End -->
+						</ul> <!-- UL End -->
+					</div> <!-- Card Body End -->
+				</div> <!-- Card End -->
+			</div>
+			<!-- Sidebar End -->
 
+		</div> <!-- Row End -->
+	</div> <!-- Container End -->
 
-			<!-- Tip Keamanan Bagi Pembeli -->
-
-		</main>
-		<!-- Footer -->
-		<?php $this->load->view("user/_partials/footer.php") ?>
-		<!-- Optional JavaScript; choose one of the two! -->
-		<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-				integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-		crossorigin="anonymous"></script>
-		<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
-		<!-- Option 2: Separate Popper and Bootstrap JS -->
-		<!--
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-		-->
-	</body>
+	<!-- Footer -->
+	<?php $this->load->view('user/_partials/footer.php'); ?>
+</body>
 </html>
