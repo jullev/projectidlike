1c36f9636a5bd5f4e2bd6bd2a36d10e8c1b9d8bc
Anggito Suryo Baskoro <e41191560@student.polije.ac.id>
2021-05-25 00:50:18 +0700

    verify not fix + table verifikasi = verify_status

--- application/views/user/verifikasiemail.php
+++ application/views/user/verifikasiemail.php
@@ -2,33 +2,45 @@
 <html lang="en">
 <!-- Head -->
 <?php $this->load->view('user/_partials/head.php'); ?>
+
 <body>
-<!-- Navbar -->
-<?php $this->load->view('user/_partials/navbar.php'); ?>
-<div class="container mb-5" style="margin-top: 100px;">
-	<div class="row">
-		<div class="col-12 col-sm-10 col-md-8 col-lg-6 mx-auto">
-			<div class="card">
-				<div class="card-header">
-					<h3>Menunggu Verifikasi Email</h3>
-				</div>
-				<div class="card-body">
-					<div class="text-center">
-						<span>Link verifikasi akun dikirim ke email anda</span>
-						<div class="mt-2">
-							<a href="" class="btn btn-primary">Kembali Ke Halaman Utama</a>
-						</div>
-						<div class="mt-4">
-							<span>Email belum masuk ?</span>
-							<a href="">Kirim ulang</a>
-						</div>
-					</div>
-				</div>
-			</div><!-- /card -->
-		</div> <!-- /col -->
-	</div> <!-- /row -->
-</div> <!-- /container -->
-<!-- Footer -->
-<?php $this->load->view('user/_partials/footer.php'); ?>
+	<div class="wrapper">
+		<!-- Navbar -->
+		<?php $this->load->view('user/_partials/navbar.php'); ?>
+		<div class="content-wrapper">
+			<section class="content container mb-5" style="margin-top: 100px;">
+				<div class="row">
+					<div class="col-12 col-sm-10 col-md-8 col-lg-6 mx-auto">
+						<div class="card">
+							<div class="card-header">
+								<h3>
+									<center> Menunggu Verifikasi Email</center>
+								</h3>
+							</div>
+							<div class="card-body">
+								<div class="text-center">
+									<span>Link verifikasi akun dikirim ke email anda</span>
+									<div class="mt-2">
+										<a href="" class="btn btn-primary">Kembali Ke Halaman Utama</a>
+									</div>
+									<div class="mt-4">
+										<span>Email belum masuk ?</span>
+										<a href="">Kirim ulang</a>
+									</div>
+								</div>
+							</div>
+						</div><!-- /card -->
+					</div> <!-- /col -->
+				</div> <!-- /row -->
+			</section>
+
+		</div>
+		<!-- /container -->
+		<!-- Footer -->
+
+	</div>
+
+	<?php $this->load->view('user/_partials/footer.php'); ?>
 </body>
+
 </html>
